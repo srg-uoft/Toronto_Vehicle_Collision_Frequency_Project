e004026fcf537ef94a0b33e377f5a09993cef7af
Stella Gregorski <stella.gregorski@mail.utoronto.ca>
2026-06-16 18:26:02 -0400

    Starting an R script file to use in making visuals, generating tables, and generating summary statistics using our anaylsis data in the initial exploratory data analysis phase. We can copy code out of this script file and into R code chunks for whichever graphs/tables/statistics we want to include in the actual Quarto documents.

---
+++ scripts/01-exploratory-data-analysis.R
@@ -0,0 +1,25 @@
+#### Preamble ####
+# Purpose: A space for generating initial summary statistics and visuals for our INF2167 project, 
+#           as a draft space before deciding which blocks of code to run in the ultimate proposal/report
+# Author: Stella Gregorski, Yiying Qin
+# Date: May-August 2026
+# Contact: stella.gregorski@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: please follow the instructions in the README of this repo to download the data ahead of time
+#                 install the tidyverse package in your local instance of R, if not already installed
+
+#### Import Required Libraries ####
+library(tidyverse)
+
+#### Read In the Dataset ####
+og_data <- read_csv("data/02 - analysis_data/analysis_data.csv")
+
+#### Space for Drafting Proposal Statistics/Tables ####
+
+
+
+#### Space for Drafting Proposal Visuals ####
+
+
+
+
